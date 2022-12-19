d202b67d95d0503cd6e503610caf311a3873b470
manojbhandi <bmanojkumar5@gmail.com>
2022-12-19 09:31:37 +0530

    some changes

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,11 +1,24 @@
 import * as React from 'react';
+import { Col, Row } from 'react-bootstrap';
+import storeItems from '../data/items.json'
 
 interface IStoreProps {
+
 }
 
 export const Store: React.FunctionComponent<IStoreProps> = (props) => {
     return (
-        <h1>Store</h1>
+        <>
+            <h1>Store</h1>
+            <Row>
+                {storeItems.map((item) => {
+                    return (
+                        <Col><StorItem item={item} /></Col>
+                    )
+                })}
+            </Row>
+
+        </>
     )
 };
 
